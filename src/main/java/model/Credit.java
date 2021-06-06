d76d6d086a63badfaaad4072b0b61225e367f42a
franziskakoellschen <85167986+franziskakoellschen@users.noreply.github.com>
2021-06-06 15:23:08 +0200

    new Calculation

--- src/main/java/model/Credit.java
+++ src/main/java/model/Credit.java
@@ -1,127 +1,119 @@
 package model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.Math;
 
 public class Credit{
-
-    private float loanAmount;
-    private float interestRate;
-    private String interestPeriod;
-    private int period;
+    private double loanAmount; // Kreditbetrag
+    private double interestRate; //Zinssatz
+    private int interestPeriod; // Laufzeit
+    //private int period;
     private String periodUoM;
-    private float repaymentAmount;
-    private String repaymentPeriod;
-    private String creditType;
-    private float endAmount;
+    private double repaymentAmount; //zurückzuzahlende Betrag
+    //private String repaymentPeriod;
+    private String paymentRhythm; // braucht man meiner Meinung nach gar nicht für die Berechnung
+    public enum creditTypes {FAELLIGKEITSKREDIT, ANNUITAETENKREDIT, ABZAHLUNGSKREDIT}
+    private creditTypes creditType; //Kreditarten
+    private double interestSum; //aufaddierten Zinsen
+    private double interest; //Zinsen der Periode
+    private double repayment; //Tilgung
+    private double annuity; //Annuität
+    private double remainingDept; //Restschuld
+    private double actInterestRate;
 
     public Credit(){
-
     }
-
-    public void setAllAtributes(float loanAmount, float interestRate, String interestPeriod, int period, String periodUoM, float repaymentAmount, String repaymentPeriod, String creditType) {
+    public void setParameters(double loanAmount, String periodUoM, double interestRate, int interestPeriod, String paymentRhythm, creditTypes creditType) {
         this.loanAmount = loanAmount;
-        this.endAmount = loanAmount;
-        this.interestRate = interestRate;
-        this.interestPeriod = interestPeriod;
-        this.period = period;
         this.periodUoM = periodUoM;
-        this.repaymentAmount = repaymentAmount;
-        this.repaymentPeriod = repaymentPeriod;
+        this.interestRate = interestRate/100;
+        this.interestPeriod = interestPeriod;
+        this.paymentRhythm = paymentRhythm;
         this.creditType = creditType;
     }
-
-    public void calculateEndAmount(){
-        int interestPeriod = 0;
-        int repaymentPeriod = 0;
-        float endAmount = 0;
-        String repaymentPeriodInWords = this.getRepaymentPeriod();
-        float repaymentAmount = this.getRepaymentAmount();
-        int period = 0;
-        float loanAmount = this.getLoanAmount();
-        float rate = this.getInterestRate();
-        switch (this.getInterestPeriod()){
-            case "monthly":
-                if(this.getRepaymentPeriod().equals("yearly")){
-                    repaymentPeriodInWords = "monthly";
-                    repaymentAmount /= 12.0;
-                }
-                interestPeriod = 1;
+    public void calculateEndAmount() {
+        interestSum = 0;
+        if(paymentRhythm == "m") {
+            actInterestRate = interestRate/12;
+        }
+        else {actInterestRate = interestRate;}
+        switch (creditType) {
+            case FAELLIGKEITSKREDIT:
+                calculateMaturityCredit();
                 break;
-            case "yearly":
-                interestPeriod = 12;
+            case ANNUITAETENKREDIT:
+                calculateAnnuityCredit();
+                break;
+            case ABZAHLUNGSKREDIT:
+                calculateInstallmentCredit();
                 break;
         }
-        switch (repaymentPeriodInWords){
-            case "monthly":
-                if(interestPeriod == 12){
-                    interestPeriod = 1;
-                    rate = (float) (100 * ((Math.pow(1 + this.getInterestRate() / 100, 1.0 / 12.0)) - 1));
-                }
-                repaymentPeriod = 1;
-                break;
-            case "yearly":
-                repaymentPeriod = 12;
-                break;
-        }
-        switch (this.getPeriodUoM()){
-            case "months":
-                period = this.getPeriod();
-                break;
-            case "years":
-                period = this.getPeriod() * 12;
-                break;
-        }
-
-        for(int i = 1; i <= period; i++){
-            if(i % interestPeriod == 0){
-                endAmount += (loanAmount * rate);
-            }
-            if(i % repaymentPeriod == 0){
-                loanAmount -= repaymentAmount;
-            }
-        }
-
-        this.setEndAmount(endAmount);
     }
-
-    public void setEndAmount(float endAmount) {
-        this.endAmount = endAmount;
+    private void calculateMaturityCredit() {
+        interestSum = loanAmount * interestRate * interestPeriod;
+        repaymentAmount = loanAmount + interestSum;
     }
-
-    public float getLoanAmount() {
+    private void calculateAnnuityCredit() {
+        double periodFactor = Math.pow(1+interestRate, interestPeriod); //Exponentialrechung
+        annuity = loanAmount * (periodFactor * interestRate)/(periodFactor-1);
+        remainingDept = loanAmount;
+        for (int i = 0; i<interestPeriod; i++){
+            interest = loanAmount * interestRate;
+            interestSum = interestSum + interest;
+            repayment = annuity - interest;
+            remainingDept = remainingDept-repayment;
+        }
+        repaymentAmount = interestSum + loanAmount;
+    }
+    private void calculateInstallmentCredit() {
+        repayment = loanAmount / interestPeriod;
+        remainingDept = loanAmount;
+        for (int i = 0; i<interestPeriod; i++) {
+            interest = remainingDept * interestRate;
+            interestSum = interestSum + interest;
+            remainingDept = remainingDept - repayment;
+        }
+        repaymentAmount = interestSum + loanAmount;
+    }
+    public double getLoanAmount() {
         return loanAmount;
     }
-
-    public float getInterestRate() {
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+    public double getInterestRate() {
         return interestRate;
     }
-
-    public String getInterestPeriod() {
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+    public int getInterestPeriod() {
         return interestPeriod;
     }
-
-    public int getPeriod() {
-        return period;
+    public void setInterestPeriod(int interestPeriod) {
+        this.interestPeriod = interestPeriod;
     }
-
-    public String getPeriodUoM() {
-        return periodUoM;
-    }
-
-    public float getRepaymentAmount() {
+    public double getRepaymentAmount() {
         return repaymentAmount;
     }
-
-    public String getRepaymentPeriod() {
-        return repaymentPeriod;
+    public void setRepaymentAmount(double repaymentAmount) {
+        this.repaymentAmount = repaymentAmount;
     }
-
-    public String getCreditType() {
+    public String getPaymentRhythm() {
+        return paymentRhythm;
+    }
+    public void setPaymentRhythm(String paymentRhythm) {
+        this.paymentRhythm = paymentRhythm;
+    }
+    public model.Credit.creditTypes getCreditType() {
         return creditType;
     }
-
-    public float getEndAmount() {
-        return endAmount;
+    public void setCreditType(model.Credit.creditTypes creditType) {
+        this.creditType = creditType;
+    }
+    public double getInterestSum() {
+        return interestSum;
+    }
+    public void setInterestSum(double interestSum) {
+        this.interestSum = interestSum;
     }
 }
